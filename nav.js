3aad0dff813808442c26028d1e22077b606d2391
avnijain647 <javni647@gmail.com>
2025-05-05 15:53:39 +0530

    p1

---
+++ nav.js
@@ -0,0 +1,31 @@
+//
+//
+// $(function() {
+//     var img = $('.rick2').html();
+//     for (var i = 0; i < 10; i++) {
+//         moar(img);
+//     }
+//     $(window).on('scroll', function() {
+//         if ($(this).scrollTop() >= $('.rick2').height() - $(this).height()) {
+//             moar(img);
+//         }
+//     });
+//     function moar(content) {
+//         $('.rick2').append(content);
+//     }
+// });
+//
+// $(function() {
+//     var img = $('.rick').html();
+//     for (var i = 0; i < 10; i++) {
+//         moar(img);
+//     }
+//     $(window).on('scroll', function() {
+//         if ($(this).scrollTop() >= $('.rick').height() - $(this).height()) {
+//             moar(img);
+//         }
+//     });
+//     function moar(content) {
+//         $('.rick').append(content);
+//     }
+// });
